3c28c1271e31e9ca9c2aa199ac972bc41181894e
jpars26 <12161003900@muz.ifsuldeminas.edu.br>
2020-02-16 00:27:46 -0300

    feat: first commit .NET

---
+++ Primeiro/Primeiro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Primeiro {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Ler dois valores inteiros, e depois mostrar na tela a soma desses números: "); 
+
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            int soma = num1 + num2;
+
+            Console.WriteLine("O resultado foi: " + soma);
+
+        }
+    }
+}
