a9827d7d270097d2f0c2b0c6191462aeede9312f
jpars26 <12161003900@muz.ifsuldeminas.edu.br>
2020-02-16 13:09:54 -0300

    feat: exercise if_and_else

---
+++ Exercicios/Ex_IF_Else/Ex_IF_Else/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ex_IF_Else {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(" Ler um número inteiro, e depois dizer se este número é negativo ou não: ");
+
+            int num = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+
+            if (num % num2 == 0 || num2 % num == 0) {
+                Console.WriteLine("Multiplos");
+            } else {
+
+                Console.WriteLine("Não são Multiplos");
+            }
+        }
+    }
+}
