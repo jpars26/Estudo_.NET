5f12a57a6d4a5ca4558a64e91db02d56d76411a4
jpars26 <12161003900@muz.ifsuldeminas.edu.br>
2020-02-16 23:51:47 -0300

    feat: static members

---
+++ Exercicios_OObj/Ex6/Ex6/Conversor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex6 {
+    class Conversor {
+
+        public static double IOF = 6.0;
+
+        public static double Covert(double dollar, double cotacao) {
+            double total = cotacao * dollar;
+            return total + total * IOF / 100;
+        }
+
+    }
+}
---
+++ Exercicios_OObj/Ex6/Ex6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+namespace Ex6 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Qual é a cotação do dólar?");
+
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quantos dólares você vai comprar?");
+
+            double dollar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = Conversor.Covert(cotacao, dollar);
+
+            Console.WriteLine("Valor a ser pago em reais = " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
