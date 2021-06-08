749001dcbacc99ecbb428e6f03d18ee38f2b24c3
João Paulo <jpars131@gmail.com>
2021-06-08 10:45:33 -0300

    Finish_PictureForms

--- FormsApp/FormsApp/Form1.cs
+++ FormsApp/FormsApp/Form1.cs
@@ -49,8 +49,10 @@ namespace FormsApp
         }
 
     
-        // Show the Open File dialog. If the user clicks OK, load the
-        // picture that the user chose
+        /* 
+           Show the Open File dialog. If the user clicks OK, load the
+           picture that the user chose
+        */
         private void showButton_Click(object sender, EventArgs e)
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
@@ -61,21 +63,38 @@ namespace FormsApp
 
         private void closeButton_Click(object sender, EventArgs e)
         {
+            this.Close();
 
         }
 
         private void setButton_Click(object sender, EventArgs e)
         {
-
+            /* 
+               Show the color dialog box. If the user clicks OK, change the
+               PictureBox control's background to the color the user chose. 
+            */
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.BackColor = colorDialog1.Color;
         }
 
         private void clearButton_Click(object sender, EventArgs e)
         {
-
+            pictureBox1.Image = null;
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
+            /* 
+               If the user selects the Stretch check box, 
+               change the PictureBox's
+               SizeMode property to "Stretch". If the user clears 
+               the check box, change it to "Normal".
+            */
+            if (checkBox1.Checked)
+
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
 
         }
     }
