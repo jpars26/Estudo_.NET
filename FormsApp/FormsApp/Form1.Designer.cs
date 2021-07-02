c8ce800d409e0d5db0f380a0c4ed1af4eaf46328
João Paulo <jpars131@gmail.com>
2021-07-01 23:26:10 -0300

    Sava Project

--- FormsApp/FormsApp/Form1.Designer.cs
+++ FormsApp/FormsApp/Form1.Designer.cs
@@ -28,8 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnClickThis = new System.Windows.Forms.Button();
-            this.lblHelloWorld = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
@@ -37,49 +35,28 @@
             this.closeButton = new System.Windows.Forms.Button();
             this.setButton = new System.Windows.Forms.Button();
             this.clearButton = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label1 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btnClickThis
-            // 
-            this.btnClickThis.Location = new System.Drawing.Point(161, 194);
-            this.btnClickThis.Name = "btnClickThis";
-            this.btnClickThis.Size = new System.Drawing.Size(75, 23);
-            this.btnClickThis.TabIndex = 0;
-            this.btnClickThis.Text = "Click This";
-            this.btnClickThis.UseVisualStyleBackColor = true;
-            this.btnClickThis.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // lblHelloWorld
-            // 
-            this.lblHelloWorld.AutoSize = true;
-            this.lblHelloWorld.Location = new System.Drawing.Point(183, 114);
-            this.lblHelloWorld.Name = "lblHelloWorld";
-            this.lblHelloWorld.Size = new System.Drawing.Size(35, 13);
-            this.lblHelloWorld.TabIndex = 1;
-            this.lblHelloWorld.Text = "label1";
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.checkBox1, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(531, 367);
             this.tableLayoutPanel1.TabIndex = 2;
             this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
@@ -87,7 +64,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(4, 332);
+            this.checkBox1.Location = new System.Drawing.Point(4, 187);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(60, 17);
             this.checkBox1.TabIndex = 1;
@@ -102,61 +79,63 @@
             this.flowLayoutPanel1.Controls.Add(this.setButton);
             this.flowLayoutPanel1.Controls.Add(this.clearButton);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(84, 332);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(269, 187);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(443, 31);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(258, 176);
             this.flowLayoutPanel1.TabIndex = 2;
             this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
             // 
             // showButton
             // 
+            this.showButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.showButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.showButton.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.showButton.Location = new System.Drawing.Point(3, 3);
             this.showButton.Name = "showButton";
             this.showButton.Size = new System.Drawing.Size(86, 23);
             this.showButton.TabIndex = 0;
             this.showButton.Text = "Show Picture";
-            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.UseVisualStyleBackColor = false;
             this.showButton.Click += new System.EventHandler(this.showButton_Click);
             // 
             // closeButton
             // 
+            this.closeButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.closeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.closeButton.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.closeButton.Location = new System.Drawing.Point(95, 3);
             this.closeButton.Name = "closeButton";
             this.closeButton.Size = new System.Drawing.Size(86, 23);
             this.closeButton.TabIndex = 1;
             this.closeButton.Text = "Close";
-            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.UseVisualStyleBackColor = false;
             this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
             // 
             // setButton
             // 
-            this.setButton.Location = new System.Drawing.Point(187, 3);
+            this.setButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.setButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.setButton.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.setButton.Location = new System.Drawing.Point(3, 32);
             this.setButton.Name = "setButton";
             this.setButton.Size = new System.Drawing.Size(86, 23);
             this.setButton.TabIndex = 2;
             this.setButton.Text = "Set color the background";
-            this.setButton.UseVisualStyleBackColor = true;
+            this.setButton.UseVisualStyleBackColor = false;
             this.setButton.Click += new System.EventHandler(this.setButton_Click);
             // 
             // clearButton
             // 
-            this.clearButton.Location = new System.Drawing.Point(279, 3);
+            this.clearButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.clearButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.clearButton.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.clearButton.Location = new System.Drawing.Point(95, 32);
             this.clearButton.Name = "clearButton";
             this.clearButton.Size = new System.Drawing.Size(86, 23);
             this.clearButton.TabIndex = 3;
             this.clearButton.Text = "Clear Picture";
-            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.UseVisualStyleBackColor = false;
             this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
-            // 
-            // pictureBox1
-            // 
-            this.tableLayoutPanel1.SetColumnSpan(this.pictureBox1, 2);
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(4, 4);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(523, 321);
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
             // 
             // openFileDialog1
             // 
@@ -165,29 +144,33 @@
     "s (*.*)|*.*";
             this.openFileDialog1.Title = "Select_Image";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(4, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(531, 367);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Controls.Add(this.lblHelloWorld);
-            this.Controls.Add(this.btnClickThis);
             this.Name = "Form1";
-            this.Text = "Picture Viewer";
+            this.Text = "Car Station";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.flowLayoutPanel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnClickThis;
-        private System.Windows.Forms.Label lblHelloWorld;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
@@ -195,9 +178,9 @@
         private System.Windows.Forms.Button closeButton;
         private System.Windows.Forms.Button setButton;
         private System.Windows.Forms.Button clearButton;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
---
+++ WebApplication2/WebApplication2/Pages/Contact.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace WebApplication2.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            string dateTime = DateTime.Now.ToShortDateString();
+            ViewData["TimeStamp"] = dateTime;
+        }
+    }
+}
